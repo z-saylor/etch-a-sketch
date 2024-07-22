c1ac83242c9eeefe3a04fbc589282f7825aa24c9
z-saylor <173121371+z-saylor@users.noreply.github.com>
2024-07-21 22:26:30 -0400

    grid set up using flexbox with event listeners on each grid-item div

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const gridContainer = document.querySelector(".grid-container");
+
+let gridSize = 16;
+
+for (let i=1; i <= gridSize; i++) {
+    rowDiv = document.createElement("div");
+    rowDiv.setAttribute("class", "grid-row");
+    gridContainer.appendChild(rowDiv);
+    
+    for (let j=1; j <= gridSize; j++) {
+        itemDiv = document.createElement("div");
+        itemDiv.setAttribute("class", "grid-item");
+        itemDiv.setAttribute("id", `${i}-${j}`);
+        rowDiv.appendChild(itemDiv);
+
+        itemDiv.addEventListener("mouseenter", (e) => {
+            console.log(e.target.id);
+            
+        });
+    }    
+}
+
