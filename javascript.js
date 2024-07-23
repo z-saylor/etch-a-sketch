785fd3ec6dafcbc13b84ac8ead9e43b83ea0d3bb
z-saylor <173121371+z-saylor@users.noreply.github.com>
2024-07-22 23:04:14 -0400

    added commented out lines to make rainbow mode

--- javascript.js
+++ javascript.js
@@ -15,6 +15,8 @@ function createGrid(gridSize) {
             rowDiv.appendChild(itemDiv);
 
             itemDiv.addEventListener("mouseenter", (e) => {
+                //const randomColor = Math.floor(Math.random()*16777215).toString(16);
+                //itemDiv.style.backgroundColor = "#" + randomColor;
                 itemDiv.classList.add("hover");
             });
         }    
