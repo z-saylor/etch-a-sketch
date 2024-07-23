dc26446734ca2fa95d3d9fd8408a9b43996778b2
z-saylor <173121371+z-saylor@users.noreply.github.com>
2024-07-22 22:53:22 -0400

    wrapped grid creation in function createGrid. added button with functionality that prompts user to create new grid of a chosen size (default of 32).

--- javascript.js
+++ javascript.js
@@ -1,21 +1,34 @@
 const gridContainer = document.querySelector(".grid-container");
 
-let gridSize = 16;
+let grid = 32;
 
-for (let i=1; i <= gridSize; i++) {
-    rowDiv = document.createElement("div");
-    rowDiv.setAttribute("class", "grid-row");
-    gridContainer.appendChild(rowDiv);
-    
-    for (let j=1; j <= gridSize; j++) {
-        const itemDiv = document.createElement("div");
-        itemDiv.setAttribute("class", "grid-item");
-        itemDiv.setAttribute("id", `${i}-${j}`);
-        rowDiv.appendChild(itemDiv);
+function createGrid(gridSize) {
+    for (let i=1; i <= gridSize; i++) {
+        rowDiv = document.createElement("div");
+        rowDiv.setAttribute("class", "grid-row");
+        gridContainer.appendChild(rowDiv);
+        
+        for (let j=1; j <= gridSize; j++) {
+            const itemDiv = document.createElement("div");
+            itemDiv.setAttribute("class", "grid-item");
+            itemDiv.setAttribute("id", `${i}-${j}`);
+            rowDiv.appendChild(itemDiv);
 
-        itemDiv.addEventListener("mouseenter", (e) => {
-            itemDiv.classList.add("hover");
-        });
-    }    
+            itemDiv.addEventListener("mouseenter", (e) => {
+                itemDiv.classList.add("hover");
+            });
+        }    
+    }
 }
 
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    grid = prompt("How big of a grid", 32);
+    const gridRows = document.querySelectorAll(".grid-row");
+        gridRows.forEach(row => {
+            row.remove();
+        });
+    createGrid(grid);
+});
+
+createGrid(grid);
